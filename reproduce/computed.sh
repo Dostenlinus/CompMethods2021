ef03ae48e9c8db2d4b72bda242631c5c33a600ad
Dostenlinus <dosten1@jhu.edu>
2021-12-14 12:09:34 -0500

    cleaned up version

--- reproduce/computed.sh
+++ reproduce/computed.sh
@@ -3,15 +3,15 @@
 scriptDir="$(dirname "$0")"
 cd "$scriptDir/.."
 
-echo '' ; echo 'Installing requirements' ; echo ''
-[[ ! -e binder/requirements.out ]] && pip install -r binder/requirements.txt | tee binder/requirements.out 
+#echo '' ; echo 'Installing requirements' ; echo ''
+#[[ ! -e binder/requirements.out ]] && pip install -r binder/requirements.txt | tee binder/requirements.out 
 
 echo '' ; echo 'Producing figures' ; echo ''
 
 cd "."
 ipython ProjectDLO.ipynb
 
-[[ -e latexdefs.tex ]] && rm -f latexdefs.tex # Delete junk file that might be created
+#[[ -e latexdefs.tex ]] && rm -f latexdefs.tex # Delete junk file that might be created
 
 #cd "Code/Python"
 #./test_Harmenbergs_method.sh
